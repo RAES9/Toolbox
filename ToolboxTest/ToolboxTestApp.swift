774d6d8d82d55d635de3fd4fb83dfcd2e4f02cfd
erivas <erivas@tribalworldwide.gt>
2022-06-30 11:08:41 -0600

    Initial Commit

---
+++ ToolboxTest/ToolboxTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToolboxTestApp.swift
+//  ToolboxTest
+//
+//  Created by Tribal on 30/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ToolboxTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
